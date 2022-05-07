b1ed965cb983ce37d766633839a27f736129fffc
Bing Guo <bing.guo@som.umaryland.edu>
2022-05-07 15:42:15 -0400

    add tests; allow normlization

--- annotate.py
+++ annotate.py
@@ -1,4 +1,4 @@
-# /usr/bin/env python3
+#! /usr/bin/env python3
 
 import pandas as pd
 from subprocess import run
@@ -7,18 +7,23 @@ import argparse
 import allel
 import re
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--vcf', type=str, required=True)
-parser.add_argument('--genome', type=str, required='PlasmoDB-44_Pfalciparum3D7')
-parser.add_argument('--out_prefix', type=str, default='out')
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--vcf", type=str, required=True, help="Input Vcf(.gz) file")
+parser.add_argument(
+    "--genome",
+    type=str,
+    default="PlasmoDB-44_Pfalciparum3D7",
+    help='Prebuilt genome used by snpEff, see "setup.sh" for details',
+)
+parser.add_argument("--out_prefix", type=str, default="out", help="output prefix")
 
 args = parser.parse_args()
 vcf = args.vcf
 genome = args.genome
 out_prefix = args.out_prefix
-out_vcf = f'{out_prefix}.vcf.gz'
-out_stats = f'{out_prefix}_stats.html'
-out_table = f'{out_prefix}.tsv'
+out_vcf = f"{out_prefix}.vcf.gz"
+out_stats = f"{out_prefix}_stats.html"
+out_table = f"{out_prefix}.tsv"
 
 
 # --- ----------run annotation -------------------------------
@@ -26,30 +31,41 @@ cmd = f""" snpEff -ud 0 {genome} {vcf} -s {out_stats} -o gatk | bgzip -c > {out_
 print(cmd)
 run(cmd, shell=True)
 
-# -------------- parser annotation -------------------------------
+# ------------ parse annotation -------------------------------
 # TODO: need to convert multi-allele site to biallelic sites
-calldata = allel.read_vcf(out_vcf, fields=['CHROM', 'POS', 'REF', 'ALT', 'variants/EFF'], alt_number=1)
+calldata = allel.read_vcf(
+    out_vcf, fields=["CHROM", "POS", "REF", "ALT", "variants/EFF"], alt_number=1
+)
 
 # extract header
-eff_header = [l for l in allel.read_vcf_headers(out_vcf).headers if 'EFF' in l][0]
+eff_header = [l for l in allel.read_vcf_headers(out_vcf).headers if "EFF" in l][0]
 eff_header = re.split("'", eff_header)[1]
-eff_header = re.split('\[', eff_header)[0]
-eff_header = eff_header.replace('(', '|').replace(' ', '').replace('|', '\t').split()
+eff_header = re.split("\[", eff_header)[0]
+eff_header = eff_header.replace("(", "|").replace(" ", "").replace("|", "\t").split()
 eff_header
 
 # extract annotation fileds
-eff_df = pd.Series(calldata['variants/EFF']).str.replace('(', '|').str.replace(')', '').str.split('|', expand=True)
-eff_df = eff_df.iloc[:, :(len(eff_header))] # remove the last two optional columns, error/warnings
+eff_df = (
+    pd.Series(calldata["variants/EFF"])
+    .str.replace("(", "|", regex=False)
+    .str.replace(")", "", regex=False)
+    .str.split("|", expand=True)
+)
+eff_df = eff_df.iloc[
+    :, : (len(eff_header))
+]  # remove the last two optional columns, error/warnings
 eff_df.columns = eff_header
 
 # variant info
-variants_df = pd.DataFrame({
-    'Chrom': calldata['variants/CHROM'],
-    'POS': calldata['variants/POS'],
-    'REF': calldata['variants/REF'],
-    'ALT': calldata['variants/ALT']
-})
+variants_df = pd.DataFrame(
+    {
+        "Chrom": calldata["variants/CHROM"],
+        "POS": calldata["variants/POS"],
+        "REF": calldata["variants/REF"],
+        "ALT": calldata["variants/ALT"],
+    }
+)
 
 # combined table
 res_df = pd.concat([variants_df, eff_df], axis=1)
-res_df.to_csv(out_table, sep='\t', index=None)
+res_df.to_csv(out_table, sep="\t", index=None)
